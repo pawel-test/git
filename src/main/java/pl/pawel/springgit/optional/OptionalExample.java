be577559c1aee36e31a695ebbd12653d36a30ced
Pawel <pwwgil@gmail.com>
2019-12-15 08:09:14 +0100

    [change] - optiona examples

--- src/main/java/pl/pawel/springgit/optional/OptionalExample.java
+++ src/main/java/pl/pawel/springgit/optional/OptionalExample.java
@@ -1,4 +1,18 @@
 package pl.pawel.springgit.optional;
 
+import java.util.Optional;
+
 public class OptionalExample {
+    public static void main(String[] args) {
+        Optional<String> maybeNick = findNick();
+        if (maybeNick.isPresent()){
+            System.out.println(maybeNick.get());
+        }
+        maybeNick.ifPresent(
+                nick -> System.out.println("mam nicka: " + nick)
+        );
+    }
+    private static Optional<String> findNick(){
+        return Optional.empty();
+    }
 }
